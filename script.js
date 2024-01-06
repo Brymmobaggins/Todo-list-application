347e43cb0fa944af0211efc243eb739ec7b3135e
brymmobaggins <ibro2tablet21@yahoo.com>
2024-01-06 17:29:40 +0100

    add delete button

--- script.js
+++ script.js
@@ -10,10 +10,24 @@ function addTask() {
         alert("Enter your task for today")
     } else {
 
+        // create list item
         const li = document.createElement("li")
         li.textContent = taskInput.value
 
+        // append list item to task list
         taskList.appendChild(li)
+
+        // create delete button
+        const deleteButton = document.createElement('button')
+        deleteButton.innerHTML = `<span class="material-symbols-outlined" style="color: red; background-color:transparent; font-size: 20px;">
+        delete
+        </span>`
+
+        deleteButton.addEventListener('click', function () {
+            taskList.removeChild(li)
+        })
+        // append delete button to list item
+        li.appendChild(deleteButton)
 
     }
     taskInput.value = ""
